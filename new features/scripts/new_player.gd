506b2518d730f2422743c36f023a920d6e9a14b1
Oli <ofcunnington@hotmail.com>
2025-02-14 23:05:07 +0100

    gravity added

--- new features/scripts/new_player.gd
+++ new features/scripts/new_player.gd
@@ -2,16 +2,27 @@ extends CharacterBody3D
 
 
 var movement_speed = 250
-var movement_velocity: Vector3 
+var movement_velocity: Vector3
+var gravity: float = 0
 
 func _physics_process(delta):
 	handle_controls(delta)
+	handle_gravity(delta)
 	
 	# Movement
 	var applied_velocity: Vector3
+	
 	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
+	applied_velocity.y = -gravity
+	
 	velocity = applied_velocity
 	move_and_slide()
+
+
+func handle_gravity(delta):
+	gravity += 25 * delta
+	if gravity >= 0 and is_on_floor():
+		gravity = 0
 
 
 func handle_controls(delta):
